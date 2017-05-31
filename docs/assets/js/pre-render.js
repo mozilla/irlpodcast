ffb7d6fdc8f17899f966b48be5ae126d7042b316
Jon Petto <jonpetto@gmail.com>
2017-05-31 16:51:38 -0500

    Updated dev build

---
+++ docs/assets/js/pre-render.js
@@ -0,0 +1,6 @@
+(function() {
+    'use strict';
+
+    // Add class to reflect javascript availability for CSS
+    document.documentElement.className = document.documentElement.className.replace(/\bno-js\b/, 'js');
+})();
