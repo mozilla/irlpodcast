f767a67d52b48ea6199e9626698d178668622bfc
Jon Petto <jonpetto@gmail.com>
2017-06-06 16:11:58 -0500

    Rebuild demo site

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,31 +1,37 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 (function(Mozilla) {
     'use strict';
 
     var episodeList = document.getElementById('episode-list');
 
-    episodeList.addEventListener('click', function(e) {
-        var targetClass = e.target.classList;
+    if (episodeList) {
+        episodeList.addEventListener('click', function(e) {
+            var targetClass = e.target.classList;
 
-        if (targetClass.contains('episode-subscribe-link')) {
-            e.preventDefault();
+            if (targetClass.contains('episode-subscribe-link')) {
+                e.preventDefault();
 
-            Mozilla.Modal.createModal(e.target, $('#' + e.target.dataset.subscribeLinks), {
-                title: 'Subscribe'
-            });
-        } else if (targetClass.contains('episode-share-link')) {
-            e.preventDefault();
+                Mozilla.Modal.createModal(e.target, $('#' + e.target.dataset.subscribeLinks), {
+                    title: 'Subscribe'
+                });
+            } else if (targetClass.contains('episode-share-link')) {
+                e.preventDefault();
 
-            Mozilla.Modal.createModal(e.target, $('#' + e.target.dataset.shareLinks), {
-                title: 'Share'
-            });
-        } else if (targetClass.contains('episode-transcript-link')) {
-            e.preventDefault();
+                Mozilla.Modal.createModal(e.target, $('#' + e.target.dataset.shareLinks), {
+                    title: 'Share'
+                });
+            } else if (targetClass.contains('episode-transcript-link')) {
+                e.preventDefault();
 
-            Mozilla.Modal.createModal(e.target, $('#' + e.target.dataset.transcript), {
-                title: 'Transcript'
-            });
-        }
+                Mozilla.Modal.createModal(e.target, $('#' + e.target.dataset.transcript), {
+                    title: 'Transcript'
+                });
+            }
 
-        e.stopPropagation();
-    });
+            e.stopPropagation();
+        });
+    }
 })(window.Mozilla);
