2e95c02f146d9f86d5930eef559b4329c329e8e6
Dave Parfitt <diparfitt@gmail.com>
2017-06-07 17:07:06 -0400

    specify --delete during aws sync
    --delete removes files from the remote if they aren't
    present on the source.

--- release.sh
+++ release.sh
@@ -7,4 +7,4 @@ docker run --rm --label=jekyll \
         --volume=$(pwd)/release:/tmp/build \
         -it -p 127.0.0.1:4000:4000 jekyll/jekyll \
         jekyll build --config _config.yml,_config-build-prod.yml --destination /tmp/build
-cd ./release && aws s3 sync . s3://irlpodcast --acl public-read --exclude "docs/*"
+cd ./release && aws s3 sync . s3://irlpodcast --acl public-read --exclude "docs/*" --delete
