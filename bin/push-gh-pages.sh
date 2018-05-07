421368ced1302229ae87f3924571f1c7b6316b15
Jon Petto <jonpetto@gmail.com>
2018-05-07 15:43:42 -0500

    Add comment to push-gh-pages.sh

--- bin/push-gh-pages.sh
+++ bin/push-gh-pages.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-git checkout $GIT_COMMIT #
+# check out latest commit to be on a HEAD-less branch
+git checkout $GIT_COMMIT
 git add public
 git ci -m "Generated from $GIT_COMMIT"
 git subtree push --prefix public origin gh-pages
