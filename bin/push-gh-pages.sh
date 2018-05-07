f73e6c1fc02759f8e620156d7756a3b048d00669
Josh Mize <jgmize@gmail.com>
2018-05-07 15:31:56 -0500

    Remove -f from git subtree push

--- bin/push-gh-pages.sh
+++ bin/push-gh-pages.sh
@@ -3,4 +3,4 @@
 git checkout $GIT_COMMIT #
 git add public
 git ci -m "Generated from $GIT_COMMIT"
-git subtree push -f --prefix public origin gh-pages
+git subtree push --prefix public origin gh-pages
