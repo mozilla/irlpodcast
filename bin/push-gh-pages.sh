b7af1b094d2839d402bdba1bc3ad01a292f0b4c5
Josh Mize <jgmize@gmail.com>
2018-05-07 16:01:19 -0500

    Use git subtree split instead of subtree push so we can force

--- bin/push-gh-pages.sh
+++ bin/push-gh-pages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# check out latest commit to be on a HEAD-less branch
-git checkout $GIT_COMMIT
+git checkout ${GIT_COMMIT:=$(git rev-parse --short HEAD)} #detached head
 git add public
 git ci -m "Generated from $GIT_COMMIT"
-git subtree push --prefix public origin gh-pages
+git subtree split --prefix public -b gh-pages-$GIT_COMMIT
+git push -f origin gh-pages-$GIT_COMMIT:gh-pages
