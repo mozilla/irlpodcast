2c134c2a1c2896c9175c96f16fa8f3cfb418cac4
Jon Petto <jonpetto@gmail.com>
2018-05-07 16:51:31 -0500

    Update Hugo build directory and comment push-gh-pages.sh.

--- bin/build.sh
+++ bin/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "$(dirname ${BASH_SOURCE[0]})"/..
-mkdir -p ./public
+mkdir -p ./release
 
 # let gulp build the assets
 docker run --rm --label=gulp \
@@ -11,7 +11,7 @@ docker run --rm --label=gulp \
 # and then let hugo build the site
 docker run --rm --label=hugo \
     --volume=$(pwd):/src \
-    --volume=$(pwd)/public:/tmp/build \
+    --volume=$(pwd)/release:/tmp/build \
     -e HUGO_ENV=production \
     jojomi/hugo:0.40.2 \
     ash -c "rm -rf /tmp/build/* && hugo --config config.toml,config-$1.toml --destination /tmp/build"
--- bin/push-gh-pages.sh
+++ bin/push-gh-pages.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# check out a detached head either from the environment var $GIT_COMMIT (which Jenkins
+# has), or using the last commit against HEAD (for local/non-Jenkins dev)
 git checkout ${GIT_COMMIT:=$(git rev-parse --short HEAD)} #detached head
-git add public
+# add files from the newly created /release folder
+git add release
+# check in the changes
 git ci -m "Generated from $GIT_COMMIT"
-git subtree split --prefix public -b gh-pages-$GIT_COMMIT
+# create a new branch named 'gh-pages-$GIT_COMMIT' with only the code from the /release folder
+git subtree split --prefix release -b gh-pages-$GIT_COMMIT
+# push our bespoke branch to the gh-pages branch
 git push -f origin gh-pages-$GIT_COMMIT:gh-pages
