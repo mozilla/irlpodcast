3a674b1c4616dd16293d478e535dea70108ddc49
Stephanie Hobson <sjhobson@gmail.com>
2019-05-10 14:06:16 -0700

    Redesign, including convert to Protocol
    - Major redesign
    - Add Protocol to dependancies and import what's needed with gulp
    - Remove Franklin Gothic font
    - Add Inter font
    - Move JS libraries to `libs` folder, don't lint libraries
    - Move intro to a partial
    - Standardize season thumbnails

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 
 'use strict';
 
+var concat = require('gulp-concat');
 var del = require('del');
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
@@ -10,9 +11,39 @@ var minify = require('gulp-minify');
 var sass = require('gulp-sass');
 var stylelint = require('gulp-stylelint');
 var argv = require('yargs').argv;
+var nodeProtocolDir = 'node_modules/@mozilla-protocol/core/protocol';
 
 // gulp build --production
 var production = !!argv.production;
+
+// copy protocol assets to public folder
+function protocolCopyUI() {
+
+    return  gulp.src(nodeProtocolDir + '/img/icons/ui/**/*.{svg,png,jpg,gif}')
+        .pipe(gulp.dest('public/images/protocol/icons/ui'));
+}
+
+// concat needed Protocol files and copy to JS directory
+function protocolJS() {
+    del(['src/js/libs/protocol.js']);
+
+    var protocolModules = [
+        'protocol-supports',
+        'protocol-utils',
+        'protocol-base',
+        'protocol-menu',
+        'protocol-modal',
+        'protocol-navigation'
+    ];
+
+    protocolModules = protocolModules.map(function(name) {
+        return nodeProtocolDir + '/js/' + name + '.js';
+    });
+
+    return gulp.src(protocolModules)
+        .pipe(concat('protocol.js'))
+        .pipe(gulp.dest('src/js/libs/'));
+}
 
 // gulp task named 'scss' that converts .scss to .css and places the .css files in static/css
 function scss() {
@@ -69,7 +100,7 @@ function js() {
 
 // lint JS files
 function jsLint() {
-    return gulp.src(['src/js/**/*.js', '!src/js/**/*.min.js'])
+    return gulp.src(['src/js/**/*.js', '!src/js/libs/**/*.js'])
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
@@ -82,12 +113,12 @@ function watch() {
 }
 
 const defaultTask = gulp.series(
-    gulp.parallel(scss, scssLint, js, jsLint),
+    gulp.series(protocolJS, protocolCopyUI, scss, scssLint, js, jsLint),
     watch
 );
 
 // build task for pushing to stage/prod
-gulp.task('build', gulp.parallel(scss, js));
+gulp.task('build', gulp.series(protocolJS, gulp.parallel(protocolCopyUI, scss, js)));
 
 gulp.task('default', defaultTask);
 module.exports = defaultTask;
