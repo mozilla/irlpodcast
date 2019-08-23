3a1ddb8c7817bf61edfcff05e0bcb50d11a1fc2e
Stephanie Hobson <sjhobson@gmail.com>
2019-08-23 13:30:34 -0700

    Season 5 Listener Survey (#228)

---
+++ src/js/survey.js
@@ -0,0 +1,27 @@
+(function($) {
+    'use strict';
+
+    var cookieName = 'survey-dismiss';
+    var hasCookies = typeof Mozilla.Cookies !== 'undefined' && Mozilla.Cookies.enabled();
+
+    var $surveyBanner;
+
+    // ensure cookies are available and the visitors has not previously dismissed the banner
+    if (hasCookies && !Mozilla.Cookies.getItem(cookieName)) {
+        $surveyBanner = $('#survey-banner');
+
+        // show the banner
+        $('#survey-dismiss').one('click', function() {
+            // don't show the banner again for 7 days
+            var d = new Date();
+            var noShowDays = 7;
+
+            d.setTime(d.getTime() + (noShowDays * 24 * 60 * 60 * 1000));
+            Mozilla.Cookies.setItem(cookieName, true, d.toUTCString(), '/');
+
+            $surveyBanner.removeClass('visible');
+        });
+
+        $surveyBanner.addClass('visible');
+    }
+})(window.jQuery);
